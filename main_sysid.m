340122210a3a82c7954e17f8be8b4c10ead1bc4e
bruderd <bruderd@umich.edu>
2019-01-22 12:55:59 -0500

    added a new data file with 11s trials, and switched to L1 error, and realized that I was combining x and y error instead of calculating them separately

--- main_sysid.m
+++ main_sysid.m
@@ -21,18 +21,18 @@ if ~exist('params' ,'var')  % recycle struct from previous run
     params = struct;
 end
 params.getData = 'file';            % ('exp' or 'file')
-params.basisID = 'poly';   % ('fourier' or 'poly' or 'fourier_sparser' or 'thinplate' or 'gaussian')
+params.basisID = 'fourier_sparser';   % ('fourier' or 'poly' or 'fourier_sparser' or 'thinplate' or 'gaussian')
 
 % parameters for reading in data (these affect how shapshot pairs built from raw data).
-params.numTrials        = 13;        % numer of sysid trials
-params.numVals          = 9;        % number of validation trials
+params.numTrials        = 16;        % numer of sysid trials
+params.numVals          = 2;        % number of validation trials
 params.Ts               = 0.1;     % sampling period
-params.K                = 155000;     % numer of snapshotPairs to take
+params.K                = 191000;     % numer of snapshotPairs to take
 params.numericalDerivs  = false;    % choose whether or not to take numerical derivatives of states (boolean)
 params.scale            = 0.9;      % scale down all state to be in range [-scale , scale]
 params.nd               = 1;        % number of delays to include in the snapshot pairs
 
-params.systemName          = 'ISR3_larm_sc09_155000pts_1delays_poly3_Ts1';  % name of current system
+params.systemName          = 'ISR3_larm_sc09_191000pts_1delays_poly3_Ts1';  % name of current system
 params.filterWindow        = floor( [1/params.Ts, 1/params.Ts] );  % if taking numerical derivatives, specifies the moving mean window before and after derivatives taken.
 
 % Koopman Sysid parameters
@@ -43,7 +43,7 @@ params.naug = params.n + params.p; % dimension of augmented state (DNE)
 params.nzeta = params.n + params.nd * (params.naug);    % dimensinon of zeta (DNE)
 
 % select maximum "degree" for basis elements
-params.maxDegree = 3;   % maximum degree of vector field monomial basis
+params.maxDegree = 2;   % maximum degree of vector field monomial basis
 
 % only do this if the Basis is not already defined. Will need to clear before running with a different basis or maxDegree
 if ~isfield(params , 'Basis')   
